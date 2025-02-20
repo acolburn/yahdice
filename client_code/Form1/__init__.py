487ff6483c68f94f0a73d1517030ba2131ac8e74
acolburn3@duck.com <acolburn3@duck.com>
2025-02-20 03:40:41 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,36 +15,21 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
     self.player_1.role=None
     self.player_2.role=None
+    self.images=[]
     
     
     
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.roll_dice(self.image_1)
 
-
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.roll_dice(self.image_2)
-
-  def button_3_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.roll_dice(self.image_3)
-
-  def button_4_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.roll_dice(self.image_4)
-
-  def button_5_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.roll_dice(self.image_5)
-
-  def button_6_click(self, **event_args):
-    """This method is called when the Roll All Dice button is clicked"""
-    images = [self.image_1,self.image_2,self.image_3,self.image_4,self.image_5]
-    for image in images:
+  def button_roll_click(self, **event_args):
+    # self.images represents dice (images) to be rolled
+    # If no dice selected, roll all the dice (images)
+    if self.images==[]:
+      self.images = [self.image_1,self.image_2,self.image_3,self.image_4,self.image_5]
+    for image in self.images:
       self.roll_dice(image)
+    self.images.clear()
+    self.button_roll.text = "ROLL ALL DICE"
 
   
   def roll_dice(self, image):
@@ -53,19 +38,16 @@ class Form1(Form1Template):
     image_number = random.choice(dice)
     time.sleep(0.2)
     image.source = image_number
-    # return image_number
+    image.visible = True
+
 
   def player_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    print('Player1 clicked')
     self.item = app_tables.table_1.get(name="player1")
     self.player_1.role="selected"
     self.player_2.role=None
     
 
   def player_2_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    print('Player2 clicked')
     self.item = app_tables.table_1.get(name="player2")
     self.player_1.role=None
     self.player_2.role="selected"
@@ -75,12 +57,33 @@ class Form1(Form1Template):
     app_tables.table_1.add_row(name="player1")
     app_tables.table_1.add_row(name="player2")
 
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  def button_new_click(self, **event_args):
+    """This method is called when the New Game button is clicked"""
     self.clear_scores()
     self.item = app_tables.table_1.get(name="player1")
     self.player_1.role="selected"
     self.player_2.role=None
+
+  def link_1_click(self, **event_args):
+   self.image_click(self.image_1)
+
+
+  def link_2_click(self, **event_args):
+    self.image_click(self.image_2)
+
+  def link_3_click(self, **event_args):
+    self.image_click(self.image_3)
+
+  def link_4_click(self, **event_args):
+    self.image_click(self.image_4)
+
+  def link_5_click(self, **event_args):
+    self.image_click(self.image_5)
+
+  def image_click(self, image):
+    image.visible = False
+    self.images.append(image)
+    self.button_roll.text="Roll Selected Dice"
 
   
 
